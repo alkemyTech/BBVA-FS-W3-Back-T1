9ef7156c357665fde4880d6afa4008d80864ab99
DiegoPerez <diego_martin_180@hotmail.com>
2023-07-04 17:50:21 -0300

    BBVAFSW3T1-19 | Eliminé la validación NonBlank en el atributo Currency

--- wallet/src/main/java/com/bbva/wallet/entities/Account.java
+++ wallet/src/main/java/com/bbva/wallet/entities/Account.java
@@ -3,7 +3,6 @@ import com.bbva.wallet.enums.Currencies;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;

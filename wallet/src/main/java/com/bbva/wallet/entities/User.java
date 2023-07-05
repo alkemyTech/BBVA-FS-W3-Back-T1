6c2ee877fb592c23f6424876bb31dd5e8028cbdb
SoleGr <sole.012@gmail.com>
2023-07-05 13:37:22 -0300

    agrego soft delete en user

--- wallet/src/main/java/com/bbva/wallet/entities/User.java
+++ wallet/src/main/java/com/bbva/wallet/entities/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,6 +23,8 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 @Setter@Getter
+@SQLDelete(sql = "UPDATE users SET soft_delete=true WHERE id=?")
+@Where(clause = "soft_delete = false")
 @Entity
 @Table(name = "users")
 public class User implements UserDetails,Serializable {

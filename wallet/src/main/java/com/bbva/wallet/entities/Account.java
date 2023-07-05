6a477a16a524b73a78ce7c752bac3c096ca0e6ef
DiegoPerez <diego_martin_180@hotmail.com>
2023-07-05 10:24:50 -0300

    Añadi generador de CBU. Corregí softDelete por defecto

--- wallet/src/main/java/com/bbva/wallet/entities/Account.java
+++ wallet/src/main/java/com/bbva/wallet/entities/Account.java
@@ -3,6 +3,7 @@ import com.bbva.wallet.enums.Currencies;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -10,6 +11,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Random;
 
 @Builder
 @NoArgsConstructor
@@ -19,6 +21,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table (name = "accounts")
 public class Account implements Serializable {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,6 +42,7 @@ public class Account implements Serializable {
     private User userId;
 
     @NotNull
+    @NotBlank
     @Column (unique = true)
     private String cbu;
 
@@ -51,8 +55,28 @@ public class Account implements Serializable {
     private LocalDateTime updateDate;
 
     @JsonIgnore
-    @Column(nullable = false ,columnDefinition = "boolean default false")
-    private Boolean softDelete;
+    @Column(name = "soft_delete", nullable = false ,columnDefinition = "boolean default false")
+    private boolean softDelete;
 
 
+
+    private static final int CBU_LENGTH = 22;
+
+    @PrePersist
+    protected void onCreate(){
+        generateCBU();
+    }
+
+    public void generateCBU() {
+        Random random = new Random();
+        StringBuilder cbuBuilder = new StringBuilder();
+
+        for (int i = 0; i < CBU_LENGTH; i++) {
+            int digit = random.nextInt(10);
+            cbuBuilder.append(digit);
+        }
+
+        this.cbu = cbuBuilder.toString();
+    }
+    
 }

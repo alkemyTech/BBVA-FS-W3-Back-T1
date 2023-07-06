16fb94a832fe2cac66923d3f8713082a9aefbffd
Evaristo00 <evaristogaston@gmail.com>
2023-07-06 10:27:40 -0300

    agregado metodo getUserAccounts en UserService

--- wallet/src/main/java/com/bbva/wallet/services/UserService.java
+++ wallet/src/main/java/com/bbva/wallet/services/UserService.java
@@ -1,12 +1,23 @@
 package com.bbva.wallet.services;
 
+import com.bbva.wallet.entities.Account;
+import com.bbva.wallet.entities.User;
 import com.bbva.wallet.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public List<Account> getUserAccounts(Long id){
+        //CAMBIAR CUANDO HAYA LAS CUSTOM EXCEPTION
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("usuario no existe"));
+
+        return user.getAccountList();
+    }
 }

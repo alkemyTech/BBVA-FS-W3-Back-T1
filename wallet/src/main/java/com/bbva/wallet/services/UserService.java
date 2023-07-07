8a5d459454e7e0277bfea215fe6624f29bbedef4
Evaristo00 <evaristogaston@gmail.com>
2023-07-07 10:57:18 -0300

    confiuracion de merge con master

--- wallet/src/main/java/com/bbva/wallet/services/UserService.java
+++ wallet/src/main/java/com/bbva/wallet/services/UserService.java
@@ -2,7 +2,6 @@ package com.bbva.wallet.services;
 
 import com.bbva.wallet.entities.Account;
 import com.bbva.wallet.entities.User;
-import com.bbva.wallet.entities.User;
 import com.bbva.wallet.exceptions.ExceptionUserAlreadyExist;
 import com.bbva.wallet.exceptions.ExceptionUserNotFound;
 import com.bbva.wallet.repositories.UserRepository;
@@ -19,8 +18,7 @@ public class UserService {
     private final UserRepository userRepository;
 
     public List<Account> getUserAccounts(Long id) {
-        //CAMBIAR CUANDO HAYA LAS CUSTOM EXCEPTION
-        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("usuario no existe"));
+        User user = userRepository.findById(id).orElseThrow(() -> new ExceptionUserNotFound());
 
         return user
                 .getAccountList().stream()

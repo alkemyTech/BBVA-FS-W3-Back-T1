95ca541147c27041e1a9cbb0e4b068f4d92e4824
DiegoPerez <diego_martin_180@hotmail.com>
2023-07-07 11:37:28 -0300

    Elimino metodo sin uso en UserRepository

--- wallet/src/main/java/com/bbva/wallet/repositories/UserRepository.java
+++ wallet/src/main/java/com/bbva/wallet/repositories/UserRepository.java
@@ -9,6 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    User findByFirstName(String name);
     Optional<User> findByEmail(String email);
 }

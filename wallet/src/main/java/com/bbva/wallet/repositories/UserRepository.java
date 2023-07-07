4e1860dada2c21759656320561f95338c66db6a6
DiegoPerez <diego_martin_180@hotmail.com>
2023-07-07 11:08:46 -0300

    Elimine importacion sin uso.

--- wallet/src/main/java/com/bbva/wallet/repositories/UserRepository.java
+++ wallet/src/main/java/com/bbva/wallet/repositories/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    public User findByFirstName(String name);
+    User findByFirstName(String name);
 }

1c0303a58aa79d2d685f4ffcb936a38849e747f6
Evaristo00 <evaristogaston@gmail.com>
2023-07-10 14:55:06 -0300

    agregado de cantiDias en dto

--- wallet/src/main/java/com/bbva/wallet/dtos/CreateFixedTermDto.java
+++ wallet/src/main/java/com/bbva/wallet/dtos/CreateFixedTermDto.java
@@ -1,8 +1,9 @@
 package com.bbva.wallet.dtos;
 
 import com.bbva.wallet.enums.Currencies;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-public record CreateFixedTermDto(@NotNull Currencies currencies,@Positive @NotNull Double balance) {
+public record CreateFixedTermDto(@NotNull Currencies currencies,@Positive @NotNull Double balance,@NotNull @Min(30) Integer cantDias) {
 }

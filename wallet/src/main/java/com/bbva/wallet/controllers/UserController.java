49b1ec005a0986560bce1f6ad47dee65fbe889b3
Evaristo00 <evaristogaston@gmail.com>
2023-07-11 13:39:05 -0300

    controller

--- wallet/src/main/java/com/bbva/wallet/controllers/UserController.java
+++ wallet/src/main/java/com/bbva/wallet/controllers/UserController.java
@@ -1,7 +1,10 @@
 package com.bbva.wallet.controllers;
 
+import com.bbva.wallet.dtos.UpdateUserDto;
+import com.bbva.wallet.entities.Account;
 import com.bbva.wallet.entities.User;
 import com.bbva.wallet.services.UserService;
+import com.bbva.wallet.utils.Response;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -30,4 +35,11 @@ public class UserController {
     }
 
 
+    @PreAuthorize("#id == authentication.principal.id")
+    @RequestMapping(value = "/{id}", method = RequestMethod.PATCH)
+    public ResponseEntity<Response> updateUser(@RequestBody UpdateUserDto dto){
+        Response<List<Account>> response = new Response<>();
+//        response.setData(userService);
+        return ResponseEntity.ok(response);
+    }
 }
---
+++ wallet/src/main/java/com/bbva/wallet/dtos/UpdateUserDto.java
@@ -0,0 +1,4 @@
+package com.bbva.wallet.dtos;
+
+public record UpdateUserDto(String nombre,String apellido,String contraseña) {
+}

a603c7a9fb090ff111d0138ba794bdd5235bd391
DiegoPerez <diego_martin_180@hotmail.com>
2023-07-11 16:03:33 -0300

    Creo excepciones para los errores usuario sin permiso, y transaccion no existe. Agrego logica en TransactionService.

--- wallet/src/main/java/com/bbva/wallet/controllers/TransactionController.java
+++ wallet/src/main/java/com/bbva/wallet/controllers/TransactionController.java
@@ -1,9 +1,12 @@
 package com.bbva.wallet.controllers;
 
+import com.bbva.wallet.dtos.TransactionDescriptionDto;
+import com.bbva.wallet.entities.Transaction;
 import com.bbva.wallet.services.TransactionService;
+import com.bbva.wallet.utils.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transactions")
@@ -11,5 +14,11 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
 
+    @PatchMapping("/{id}")
+    public ResponseEntity<Response>editTransaction(@PathVariable Long id, @RequestBody TransactionDescriptionDto transactionDescriptionDto){
+        Response <Transaction> response = new Response<>();
+        response.setData(transactionService.editTransaction(id, transactionDescriptionDto.getDescription()));
+        return ResponseEntity.ok(response);
+    }
 
 }
--- wallet/src/main/java/com/bbva/wallet/services/TransactionService.java
+++ wallet/src/main/java/com/bbva/wallet/services/TransactionService.java
@@ -1,6 +1,14 @@
 package com.bbva.wallet.services;
 
+import com.bbva.wallet.entities.Account;
+import com.bbva.wallet.entities.Transaction;
+import com.bbva.wallet.entities.User;
+import com.bbva.wallet.exceptions.ExceptionTransactionNotExist;
+import com.bbva.wallet.exceptions.ExceptionUserNotAuthenticated;
+import com.bbva.wallet.exceptions.ExceptionUserNotFound;
 import com.bbva.wallet.repositories.TransactionRepository;
+import com.bbva.wallet.repositories.UserRepository;
+import com.bbva.wallet.utils.ExtractUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +16,22 @@ import org.springframework.stereotype.Service;
 public class TransactionService {
     @Autowired
     private TransactionRepository transactionRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    public Transaction editTransaction(Long id, String description){
+
+        User authenticatedUser = userRepository.findById(ExtractUser.extract().getId())
+                .orElseThrow(ExceptionUserNotAuthenticated::new);
+
+        Transaction transactionToEdit = transactionRepository.findById(id)
+                .orElseThrow(ExceptionTransactionNotExist::new);
+
+        if(transactionToEdit.getAccount().getUserId().getId().equals(authenticatedUser.getId())){
+            transactionToEdit.setDescription(description);
+            return transactionRepository.save(transactionToEdit);
+        } else {
+            throw new ExceptionUserNotAuthenticated();
+        }
+    }
 }

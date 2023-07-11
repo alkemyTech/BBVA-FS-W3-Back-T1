5be0712b3eab5221a148bba7ef49a0999c73bb93
Evaristo00 <evaristogaston@gmail.com>
2023-07-11 11:58:41 -0300

    controller y service de FixedTerm

---
+++ wallet/src/main/java/com/bbva/wallet/controllers/FixedTermController.java
@@ -0,0 +1,33 @@
+package com.bbva.wallet.controllers;
+
+import com.bbva.wallet.dtos.CreateFixedTermDto;
+import com.bbva.wallet.entities.Account;
+import com.bbva.wallet.entities.FixedTermDeposit;
+import com.bbva.wallet.entities.User;
+import com.bbva.wallet.services.FixedTermService;
+import com.bbva.wallet.utils.Response;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/fixedTerm")
+public class FixedTermController {
+
+    private final FixedTermService fixedTermService;
+
+    @PostMapping
+    public ResponseEntity<Response> simulateFixedTerm(){
+        Response<Account> response = new Response<>();
+//        response.setData();
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ wallet/src/main/java/com/bbva/wallet/services/FixedTermService.java
@@ -0,0 +1,4 @@
+package com.bbva.wallet.services;
+
+public class FixedTermService {
+}

aedf4747df2f699f37374ac901814388a242c247
DiegoPerez <diego_martin_180@hotmail.com>
2023-07-11 13:24:56 -0300

    creo las carpetas y archivos de transaction

---
+++ wallet/src/main/java/com/bbva/wallet/controllers/TransactionController.java
@@ -0,0 +1,15 @@
+package com.bbva.wallet.controllers;
+
+import com.bbva.wallet.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+    @Autowired
+    private TransactionService transactionService;
+
+
+}
---
+++ wallet/src/main/java/com/bbva/wallet/services/TransactionService.java
@@ -0,0 +1,11 @@
+package com.bbva.wallet.services;
+
+import com.bbva.wallet.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+    @Autowired
+    private TransactionRepository transactionRepository;
+}

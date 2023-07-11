397e02db5bed85da1f5834ad929ab764d316b20c
DiegoPerez <diego_martin_180@hotmail.com>
2023-07-11 19:12:26 -0300

    Corrijo error en la anotacion @RestController (Estaba como @Controller).

--- wallet/src/main/java/com/bbva/wallet/controllers/TransactionController.java
+++ wallet/src/main/java/com/bbva/wallet/controllers/TransactionController.java
@@ -1,20 +1,20 @@
 package com.bbva.wallet.controllers;
 
 import com.bbva.wallet.entities.Transaction;
-import com.bbva.wallet.entities.User;
 import com.bbva.wallet.services.TransactionService;
 import com.bbva.wallet.utils.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/transactions")
 public class TransactionController {
     @Autowired

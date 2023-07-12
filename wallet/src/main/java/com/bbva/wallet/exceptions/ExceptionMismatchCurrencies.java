633d5bacb3898ebe806f45f872c107e90915ecf9
Horacio Amaro <melodyamaro@gmail.com>
2023-07-12 04:12:39 -0300

    BBVAFSW3T1-37 | Endpoint para realizar un pago

--- wallet/src/main/java/com/bbva/wallet/exceptions/ExceptionMismatchCurrencies.java
+++ wallet/src/main/java/com/bbva/wallet/exceptions/ExceptionMismatchCurrencies.java
@@ -5,6 +5,6 @@ import com.bbva.wallet.enums.ErrorCodes;
 public class ExceptionMismatchCurrencies extends CustomException {
 
     public ExceptionMismatchCurrencies() {
-        super("El tipo de moneda del dinero que queres enviar no coincide con el tipo de moneda de la cuenta destinataria",ErrorCodes.NO_COINCIDEN_TIPO_DE_MONEDAS);
+        super("El tipo de moneda del monto no coincide con el tipo de moneda de la cuenta",ErrorCodes.NO_COINCIDEN_TIPO_DE_MONEDAS);
     }
 }

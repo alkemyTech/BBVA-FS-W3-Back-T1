a9f61e737354601392105e4195137626a6d4e7c0
Evaristo00 <evaristogaston@gmail.com>
2023-07-12 09:33:35 -0300

    devuelve Hateos pero con size y demasiados links

--- wallet/src/main/java/com/bbva/wallet/controllers/UserController.java
+++ wallet/src/main/java/com/bbva/wallet/controllers/UserController.java
@@ -1,21 +1,13 @@
 package com.bbva.wallet.controllers;
 
-import com.bbva.wallet.dtos.UpdateUserDto;
 import com.bbva.wallet.entities.User;
 import com.bbva.wallet.services.UserService;
 import com.bbva.wallet.utils.Response;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.bind.annotation.*;
-
 
 import java.util.Optional;
 
@@ -25,6 +17,11 @@ import java.util.Optional;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserModelAssembler userModelAssembler;
+
+     @Autowired
+     private PagedResourcesAssembler<User> pagedResourcesAssembler;
 
     @PreAuthorize("hasAuthority('ADMIN') || #id == authentication.principal.id")
     @DeleteMapping("/{id}")
@@ -35,9 +32,13 @@ public class UserController {
     @GetMapping
     public ResponseEntity<Response> getAll(@RequestParam(required = false) Optional<Integer> page) {
         Response response = new Response<>();
-
+        CollectionModel<UserModel> collectionModel;
         if (page.isPresent()) {
-            response.setData(userService.getTen(page.get()));
+            Page<User> pagedEntity = userService.getTen(page.get());
+            PagedModel<UserModel> pagedModel = pagedResourcesAssembler.toModel(pagedEntity,userModelAssembler);
+            collectionModel = CollectionModel.of(pagedModel.getContent());
+//            collectionModel.add(pagedModel.getLinks());
+            response.setData(collectionModel);
         }
         else {
             response.setData(userService.getAll());
@@ -63,4 +64,6 @@ public class UserController {
         response.setData(userService.getUser(user.getId()));
         return ResponseEntity.ok(response);
     }
+
+
 }
--- wallet/src/main/java/com/bbva/wallet/services/UserService.java
+++ wallet/src/main/java/com/bbva/wallet/services/UserService.java
@@ -81,7 +81,7 @@ public class UserService {
         return userRepository.findById(id);
     }
 
-    public Slice<User> getTen(Integer page) {
+    public Page<User> getTen(Integer page) {
         return userRepository.findSliceByPage(PageRequest.of(page, 2));
     }
 

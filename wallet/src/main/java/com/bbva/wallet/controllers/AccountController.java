91bb3cd2beed8e2bc41843ad55260f5920bd51f0
DiegoPerez <diego_martin_180@hotmail.com>
2023-07-13 16:03:11 -0300

    Agrego OpenApiConfig, asigno Tag u @Operations en endpoints de AccountController.

--- wallet/src/main/java/com/bbva/wallet/controllers/AccountController.java
+++ wallet/src/main/java/com/bbva/wallet/controllers/AccountController.java
@@ -8,13 +8,17 @@ import com.bbva.wallet.entities.User;
 import com.bbva.wallet.services.AccountService;
 import com.bbva.wallet.services.UserService;
 import com.bbva.wallet.utils.Response;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;

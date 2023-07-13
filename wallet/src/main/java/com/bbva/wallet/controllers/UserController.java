1f063224d38bfce243b6d92cbd77642aa1d97c4c
Evaristo00 <evaristogaston@gmail.com>
2023-07-13 15:42:41 -0300

    postman

--- wallet/src/main/java/com/bbva/wallet/controllers/UserController.java
+++ wallet/src/main/java/com/bbva/wallet/controllers/UserController.java
@@ -1,12 +1,10 @@
 package com.bbva.wallet.controllers;
 
 import com.bbva.wallet.dtos.UpdateUserDto;
-import com.bbva.wallet.entities.Account;
 import com.bbva.wallet.entities.User;
-import com.bbva.wallet.services.UserService;
-import com.bbva.wallet.utils.Response;
-import com.bbva.wallet.hateoas.UserModel;
 import com.bbva.wallet.hateoas.GenericModelAssembler;
+import com.bbva.wallet.hateoas.UserModel;
+import com.bbva.wallet.services.UserService;
 import com.bbva.wallet.utils.Response;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -16,15 +14,9 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-
-import java.util.List;
 
 
 @RestController

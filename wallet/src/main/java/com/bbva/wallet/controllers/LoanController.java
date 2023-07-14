85f242a6e8f0822aafbd022e412c1732b3846d2d
DiegoPerez <diego_martin_180@hotmail.com>
2023-07-14 12:36:46 -0300

    Añado Swagger a loanController

--- wallet/src/main/java/com/bbva/wallet/controllers/LoanController.java
+++ wallet/src/main/java/com/bbva/wallet/controllers/LoanController.java
@@ -2,18 +2,47 @@ package com.bbva.wallet.controllers;
 
 import com.bbva.wallet.dtos.LoanDto;
 import com.bbva.wallet.dtos.LoanRequestBodyDto;
+import com.bbva.wallet.entities.Account;
 import com.bbva.wallet.services.LoanService;
+import com.bbva.wallet.utils.Response;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "400", description = "Custom Error", content = {
+                @Content(schema = @Schema(implementation = Response.class), mediaType = "application/json")
+        }),
+        @ApiResponse(responseCode = "403", description = "No autenticado / Token inválido", content = @Content)
+})
+@Tag(name = "Accounts")
 @RestController
 @RequestMapping("/loan")
 public class LoanController {
     @Autowired
     private LoanService loanService;
+
+    @Operation(
+            description = "Endpoint accesible a usuarios autenticados",
+            summary = "Simula un préstamo",
+            responses ={
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = {
+                                    @Content(schema = @Schema(implementation = LoanDto.class), mediaType = "application/json")
+                            }
+                    )
+            }
+    )
     @PostMapping("/simulate")
     public LoanDto simulateLoan(@RequestBody LoanRequestBodyDto loanRequestBodyDto){
         return loanService.simulateLoan(loanRequestBodyDto.getAmount(), loanRequestBodyDto.getMonths());
--- wallet/src/main/java/com/bbva/wallet/dtos/LoanRequestBodyDto.java
+++ wallet/src/main/java/com/bbva/wallet/dtos/LoanRequestBodyDto.java
@@ -1,11 +1,14 @@
 package com.bbva.wallet.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class LoanRequestBodyDto {
+    @Schema(description = "Plazo en meses (Mayor a 0)", example = "12")
     private Integer months;
+    @Schema(description = "Cantidad de dinero (Mayor a 0)", example = "10000")
     private Double amount;
 }

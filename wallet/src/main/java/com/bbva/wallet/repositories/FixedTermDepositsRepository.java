fc0e25b5c8675734d18cf1bb26fc4a4c29a722b6
Evaristo00 <evaristogaston@gmail.com>
2023-07-28 12:31:57 -0300

    vacio la tabla de transactions antes de correr el seeder

--- wallet/src/main/java/com/bbva/wallet/repositories/FixedTermDepositsRepository.java
+++ wallet/src/main/java/com/bbva/wallet/repositories/FixedTermDepositsRepository.java
@@ -1,10 +1,17 @@
 package com.bbva.wallet.repositories;
 
 import com.bbva.wallet.entities.FixedTermDeposit;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FixedTermDepositsRepository extends JpaRepository<FixedTermDeposit,Long> {
 
+    @Query(value = "DELETE FROM fixed_term_deposits", nativeQuery = true)
+    @Modifying
+    @Transactional
+    void deleteAll();
 }

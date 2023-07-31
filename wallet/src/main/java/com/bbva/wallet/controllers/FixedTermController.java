ae409b9071ae33af62eb93c2b3897b87206903dc
DiegoPerez <diego_martin_180@hotmail.com>
2023-07-31 12:47:45 -0300

    Agrego test para fixedTerm

--- wallet/src/main/java/com/bbva/wallet/controllers/FixedTermController.java
+++ wallet/src/main/java/com/bbva/wallet/controllers/FixedTermController.java
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,7 +54,7 @@ public class FixedTermController {
         User user= (User) authentication.getPrincipal();
         Response<FixedTermDeposit> response = new Response<>();
         response.setData(fixedTermService.createFixedTermDeposit(dto,user));
-        return ResponseEntity.ok(response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @Operation(
--- wallet/src/test/java/com/bbva/wallet/controllers/FixedTermControllerTest.java
+++ wallet/src/test/java/com/bbva/wallet/controllers/FixedTermControllerTest.java
@@ -1,37 +1,82 @@
 package com.bbva.wallet.controllers;
 
 import com.bbva.wallet.dtos.FixedTermCreateRequestDTO;
+import com.bbva.wallet.dtos.FixedTermSimulateResponseDTO;
 import com.bbva.wallet.services.FixedTermService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.MockitoAnnotations.initMocks;
+
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.sql.Timestamp;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
 public class FixedTermControllerTest {
-    @Autowired
+
     private MockMvc mockMvc;
 
     @Mock
     private FixedTermService fixedTermService;
 
-    @Test
-    @WithAnonymousUser
-    public void testSimulateFixedTerm() throws Exception {
-        FixedTermCreateRequestDTO fixedTermCreateRequestDTO =
-                new FixedTermCreateRequestDTO(100D, 30);
+    @InjectMocks
+    private FixedTermController fixedTermController;
 
-        mockMvc.perform(post("http://localhost:8080/fixedTerm/simulate")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{ \"amount\": 2000, \"cantDias\": 2000 }"))
-                .andExpect(status().isForbidden());
+    private ObjectMapper objectMapper;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+
+        objectMapper = new ObjectMapper();
+        mockMvc = MockMvcBuilders.standaloneSetup(fixedTermController).build();
+    }
+
+    @Test
+    public void FixedTermControler_SimulateFixedTerm_ReturnCreated() throws Exception {
+        // Simular el comportamiento del servicio
+//        FixedTermCreateRequestDTO requestDTO = new FixedTermCreateRequestDTO(100D, 30);
+//
+//        Timestamp fechaCreacion = new Timestamp(System.currentTimeMillis());
+//        Timestamp fechaFinalizacion = new Timestamp(System.currentTimeMillis() + (30L * 24 * 60 * 60 * 1000)); // Agregar 30 días al tiempo actual
+//        Double montoInvertido = 1000.0;
+//        Double interes = 50.0;
+//        Double montoTotal = montoInvertido + interes;
+//
+//        FixedTermSimulateResponseDTO responseDTO =
+//                new FixedTermSimulateResponseDTO(fechaCreacion, fechaFinalizacion, montoInvertido, interes, montoTotal);
+//
+//        given(fixedTermService.simulateFixedTerm(ArgumentMatchers.any())).willReturn(responseDTO);
+
+        // Realizar la solicitud a través de MockMvc
+        ResultActions response = mockMvc.perform(post("/fixedTerm/simulate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"amount\": 2000, \"cantDias\": 30 }"));
+
+        // Verificar el resultado esperado
+        response.andExpect(status().isOk());
+    }
+
+    @Test
+    public void FixedTermControler_SimulateFixedTerm_Return400() throws Exception {
+
+        // Realizar la solicitud a través de MockMvc
+        ResultActions response = mockMvc.perform(post("/fixedTerm/simulate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"amount\": 2000, \"cantDias\": 29 }"));
+
+        // Verificar el resultado esperado
+        response.andExpect(status().isBadRequest());
     }
 }
